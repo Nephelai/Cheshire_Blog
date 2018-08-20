30f700d46a364064b3b287bf890a84704560d2eb
Cheshire <jaeho406@gmail.com>
2018-08-20 23:23:19 +0900

    set view logic.

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -37,13 +37,14 @@ var UserSchema = new Schema({
     }
 });
 
-UserSchema.virtual('fullName').get(function () {
+// Not Using fullName
+/*UserSchema.virtual('fullName').get(function () {
     return this.firstName + ' ' + this.lastName;
 }).set(function (fullName) {
     var splitName = fullName.split(' ');
     this.firstName = splitName[0] || '';
     this.lastName = splitName[1] || '';
-});
+});*/
 
 UserSchema.pre('save', function (next) {
     if(this.password) {
